8e7d6cd23c78b8bf38ee8d8bff2edcc573039fa0
jayram10125 <jayram.10125@gmail.com>
2024-01-29 22:02:40 +0530

    addition in C

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter first number : ");
+    scanf("%d",&a);
+    printf("enter second number : ");
+    scanf("%d",&b);
+    printf("the sum of %d and %d ic %d",a,b,a+b);
+    return 0;
+}
